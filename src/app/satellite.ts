7bfb8fe7d84ceb7928c44cd7dc3622c02a2d5f8e
Kendulum <ken7ma@gmail.com>
2022-12-12 20:20:12 -0600

    GA6

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -14,9 +14,13 @@ export class Satellite {
 		this.operational = operational;
    }
 	
-	isSpaceDebris(): boolean {
-		return true;
-   }
+   // TODO: Repair the isSpaceDebris method in the Satellite class.
+   // TODO: isSpaceDebris returns true if the satellite type is 'Space Debris', and it returns false otherwise.
+   isSpaceDebris(): boolean {
+	if(this.type.includes("Space Debris")) {
+	   return true;
+	} return false;
+}
 
 }
 
